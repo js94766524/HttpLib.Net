0b9a0b71e066c9531ee13e73e27f94b89516c2b2
jackstone <94766524@qq.com>
2017-08-21 09:21:14 +0800

    first upload

---
+++ HttpLib/HttpMethods.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace HttpLib
+{
+    public static class HttpMethods
+    {
+        /// <summary>
+        /// Http Request Get
+        /// </summary>
+        public static string Get(string url)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream myResponseStream = response.GetResponseStream())
+            using (StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8))
+            {
+                return myStreamReader.ReadToEnd();
+            }
+
+        }
+
+        /// <summary>
+        /// Http Request Post
+        /// </summary>
+        public static string Post(string url, string dataStr)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(dataStr);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.LongLength;
+
+            using (Stream myRequestStream = request.GetRequestStream())
+            {
+                myRequestStream.Write(data, 0, data.Length);
+                myRequestStream.Flush();
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream myResponseStream = response.GetResponseStream())
+                using (StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8))
+                {
+                    return myStreamReader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 片段下载，用于断点续传功能
+        /// </summary>
+        public static void Download(this string url, IDownloadHandler handler, long from = 0, long to = -1)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            if (to < from)
+            {
+                request.AddRange(from);
+            }
+            else
+            {
+                request.AddRange(from, to);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    throw new Exception("服务器不支持断点续传");
+                }
+                Debug.WriteLine("contentLength = " + response.ContentLength);
+                handler.setFileLength(response.ContentLength);
+                byte[] buffer = new byte[1024 * 5];
+                using (Stream stream = response.GetResponseStream())
+                {
+                    int count;
+                    //读取时注意实际接收数据大小  
+                    while ((count = stream.Read(buffer, 0, buffer.Length)) != 0 && handler.Downloading)
+                    {
+                        handler.HandleBytes(buffer, count);
+                        Thread.Sleep(5);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取请求内容的长度
+        /// </summary>
+        public static long GetContentLength(this string url)
+        {
+            long length = 0;
+            try
+            {
+                var req = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+                req.Method = "HEAD";
+                req.Timeout = 5000;
+                var res = (HttpWebResponse)req.GetResponse();
+                if (res.StatusCode == HttpStatusCode.OK)
+                {
+                    length = res.ContentLength;
+                }
+                res.Close();
+                return length;
+            }
+            catch (WebException)
+            {
+                return 0;
+            }
+        }
+    }
+
+    public interface IDownloadHandler
+    {
+        void setFileLength(long remineLength);
+        bool Downloading { get; }
+        void HandleBytes(byte[] data, int count);
+    }
+}
---
+++ HttpLib/ResumableDownload.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HttpLib
+{
+    /// <summary>
+    /// 可断点续传的异步单线程文件下载工具类
+    /// </summary>
+    public class ResumableDownload : IDownloadHandler
+    {
+        private long _startPosition;
+        private FileStream _fileStream;
+        private Thread _workThread;
+
+        public ResumableDownload(string url, string filePath)
+        {
+            if (!Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+                throw new ArgumentException(url);
+
+            URL = url;
+            FilePath = filePath;
+        }
+        
+        /// <summary>
+        /// 文件本地路径
+        /// </summary>
+        public string FilePath { get; }
+        /// <summary>
+        /// 文件网络地址
+        /// </summary>
+        public string URL { get; set; }
+        /// <summary>
+        /// 文件总长度
+        /// </summary>
+        public long FileLength { get; private set; }
+        /// <summary>
+        /// 已经下载的文件长度
+        /// </summary>
+        public long DoneLength { get; private set; }
+        /// <summary>
+        /// 下载进度百分比
+        /// </summary>
+        public double Progress { get; private set; }
+        /// <summary>
+        /// 指示下载是否在进行中
+        /// </summary>
+        public bool Downloading { get; private set; }
+
+        /// <summary>
+        /// 下载完毕事件
+        /// </summary>
+        public event EventHandler Finished;
+
+        public event ProgressEventHandler ProgressChanged;
+
+        /// <summary>
+        /// 启动下载
+        /// </summary>
+        public void StartDownload()
+        {
+            if (Downloading) return;
+            Downloading = true;
+
+            InitialResumeParams();
+            _workThread = new Thread(new ThreadStart(() =>
+            {
+                try
+                {
+                    if(FileLength != 0 && _startPosition >= FileLength)
+                    {
+                        return;
+                    }
+
+                    URL.Download(this, _startPosition);
+                    if(Progress == 100) Finished?.Invoke(this, new EventArgs());
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("下载过程中出现异常", e);
+                }
+                finally
+                {
+                    Downloading = false;
+                    _fileStream?.Close();
+                }
+            }));
+            _workThread.Start();
+        }
+
+        /// <summary>
+        /// 初始化下载所需要的参数和文件流对象
+        /// </summary>
+        private void InitialResumeParams()
+        {
+            if (File.Exists(FilePath))
+            {
+                _fileStream = File.OpenWrite(FilePath);
+                _startPosition = _fileStream.Length;
+                _fileStream.Seek(_startPosition, SeekOrigin.Current);
+            }
+            else
+            {
+                _startPosition = 0;
+                _fileStream = new FileStream(FilePath, FileMode.Create);
+            }
+            DoneLength = _startPosition;
+        }
+
+        /// <summary>
+        /// 处理下载的数据片段
+        /// </summary>
+        public void HandleBytes(byte[] data, int count)
+        {
+            _fileStream?.Write(data, 0, count);
+            DoneLength += count;
+            Progress = 100.0 * DoneLength / FileLength;
+            ProgressChanged?.Invoke(this);
+        }
+
+        /// <summary>
+        /// 计算文件总长度
+        /// </summary>
+        /// <param name="remineLength"></param>
+        public void setFileLength(long remineLength)
+        {
+            FileLength = DoneLength + remineLength;
+        }
+
+        /// <summary>
+        /// 停止下载
+        /// </summary>
+        public void Shutdown()
+        {
+            Downloading = false;
+        }
+
+        /// <summary>
+        /// 重写ToString方法，配合Parse静态方法
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return URL + "," + FilePath;
+        }
+
+        /// <summary>
+        /// 解析"URL,FilePath"格式的字符串为断线续传下载对象
+        /// </summary>
+        public static ResumableDownload Parse(string str)
+        {
+            string[] strs = str.Split(',');
+            if (strs.Length != 3) return null;
+            var r = new ResumableDownload(strs[0], strs[1]);
+            r.FileLength = long.Parse(strs[2]);
+            return r;
+        }
+
+        
+    }
+
+    public delegate void ProgressEventHandler(ResumableDownload sender);
+
+    
+}
