d581670b3435d81de6dc5e53d94ac989a04af98d
jackstone <94766524@qq.com>
2017-09-12 10:40:05 +0800

    对远程代理操作进行了简单的封装，添加了RemotingServer和RemotingClient，避免记忆繁琐的操作和设置，使用方便

---
+++ HttpLib/Remoting/Client/RemotingClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+
+namespace HttpLib.Remoting.Client
+{
+    public static class RemotingClient
+    {
+        private static bool ChannelRegistered;
+
+        /// <summary>
+        /// 注册TCP信道
+        /// </summary>
+        private static void RegisterChannel()
+        {
+            try
+            {
+                ChannelServices.RegisterChannel(new TcpClientChannel(), true);
+                ChannelRegistered = true;
+            }
+            catch
+            {
+                ChannelRegistered = false;
+            }
+        }
+
+        /// <summary>
+        /// 获取服务器端的远程对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="prefixUri">服务端地址，例如“tcp://localhost:6666/”</param>
+        /// <param name="objectUri">类型地址，例如“RemotingObject”</param>
+        /// <returns>服务器对象的代理</returns>
+        public static T GetRemotingObj<T>( string prefixUri, string objectUri = null ) where T : MarshalByRefObject
+        {
+            if (!ChannelRegistered) RegisterChannel();
+            Type t = typeof(T);
+            if (objectUri == null) objectUri = t.Name;
+            string url = (prefixUri.StartsWith("tcp://") ? "" : "tcp://") + prefixUri + (prefixUri.EndsWith("/") ? "" : "/") + objectUri;
+            try
+            {
+                return Activator.GetObject(t, url) as T;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+    }
+}
---
+++ HttpLib/Remoting/Server/RemotingServer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+
+namespace HttpLib.Remoting.Server
+{
+    public static class RemotingServer
+    {
+        /// <summary>
+        /// 注册TCP信道
+        /// </summary>
+        /// <param name="port">端口号</param>
+        /// <returns>是否注册成功</returns>
+        public static bool RegisterChannel( int port )
+        {
+            TcpServerChannel channel = new TcpServerChannel(port);
+            try
+            {
+                ChannelServices.RegisterChannel(channel, true);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 注册已知服务器类型
+        /// </summary>
+        /// <typeparam name="T">要注册的类型</typeparam>
+        /// <param name="uri">对象地址，如果为null，则默认为类名</param>
+        /// <param name="mode">默认为SingleCall模式</param>
+        public static bool RegisterObject<T>( string uri = null, WellKnownObjectMode mode = WellKnownObjectMode.SingleCall ) where T : MarshalByRefObject
+        {
+            Type t = typeof(T);
+            if (uri == null) uri = t.Name;
+            try
+            {
+                RemotingConfiguration.RegisterWellKnownServiceType(t, uri, mode);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
